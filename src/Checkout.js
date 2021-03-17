ee0dab865dbf285533e5604ea1a273f58c54f6a5
Asen Sanjula <asensanjula@gmail.com>
2021-03-17 12:04:59 +0530

    add checkout page and remove from basket functionality

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './Checkout.css'
 import Subtotal from "./Subtotal";
+import CheckoutProduct from "./CheckoutProduct";
+import {useStateValue} from "./StateProvider";
 
 function Checkout() {
+
+    const [{basket}, dispatch] = useStateValue();
     return (
         <div className='checkout'>
             <div className="checkout__left">
@@ -15,10 +19,20 @@ function Checkout() {
                         Your Shopping Basket
                     </h2>
                 </div>
+
+                {basket?.map(item => (
+                    <CheckoutProduct
+                        id={item.id}
+                        title={item.title}
+                        image={item.image}
+                        price={item.price}
+                        rating={item.rating}
+                    />
+                ))}
             </div>
 
             <div className="checkout__right">
-               <Subtotal/>
+                <Subtotal/>
             </div>
         </div>
     );
--- src/Home.js
+++ src/Home.js
@@ -14,12 +14,14 @@ function Home() {
 
             <div className="home__row">
                 <Product
+                    id={1}
                     title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
                     image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
                     price={19.99}
                     rating={4}
                 />
                 <Product
+                    id={2}
                     title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
                     image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
                     price={20.0}
@@ -28,18 +30,21 @@ function Home() {
             </div>
             <div className="home__row">
                 <Product
+                    id={3}
                     title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
                     image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
                     price={100}
                     rating={3}
                 />
                 <Product
+                    id={4}
                     title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
                     image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
                     price={98.99}
                     rating={3}
                 />
                 <Product
+                    id={5}
                     title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
                     image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
                     price={598}
@@ -48,6 +53,7 @@ function Home() {
             </div>
             <div className="home__row">
                 <Product
+                    id={6}
                     title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
                     image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
                     price={1094.99}
