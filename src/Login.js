a910d903708ed7071d0171eb1efa1df6038e5043
Asen Sanjula <asensanjula@gmail.com>
2021-03-22 23:18:19 +0530

    add login page layout

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React , { useState }from 'react';
 import './Login.css'
 import {Link} from "react-router-dom";
 
 function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = (e) => {
+        e.preventDefault();
+    }
+
+    const register = (e) => {
+        e.preventDefault();
+
+        //  firebase stuff for register
+    }
+
     return (
         <div className='login'>
             <Link to={'/'}>
@@ -17,8 +31,31 @@ function Login() {
                 <h1>Sign In</h1>
 
                 <form>
+                    <h5>E-mail</h5>
+                    <input
+                        type="text"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+
+                    <h5>Password</h5>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+
+                    <button className='login__signInButton' onClick={signIn}>Sign In</button>
 
                 </form>
+                <p>
+                    By signing-in you agree to Amazon's Conditions of Use & Sale. Please see our Privacy Notice, our
+                    Cookies Notice and our Interest-Based Ads Notice.
+                </p>
+
+                <button className='login__registerButton' onClick={register}>
+                    Create your Amazon Account
+                </button>
             </div>
 
         </div>
