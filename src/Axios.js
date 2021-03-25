6972a78cbb76484d3cde5e6e1f8c9c69cef5a73a
Asen Sanjula <asensanjula@gmail.com>
2021-03-26 00:43:02 +0530

    firebase cloud function url

--- src/Axios.js
+++ src/Axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    baseURL:'http://localhost:5001/clone-cf06a/us-central1/api'
+    baseURL:'https://us-central1-clone-cf06a.cloudfunctions.net/api'
+        //'http://localhost:5001/clone-cf06a/us-central1/api'
 });
 
 export default instance;
